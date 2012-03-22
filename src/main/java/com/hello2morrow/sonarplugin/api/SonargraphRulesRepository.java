5ff2e4cfe923776a7bd6b43891dccf92bbd60a23
Ingmar Kellner <i.kellner@hello2morrow.com>
2012-03-22 12:49:21 +0000

    SONARPLUGINS-1710: Rules cannot be loaded by Sonar 2.14
    Added description to rules.

--- src/main/java/com/hello2morrow/sonarplugin/api/SonargraphRulesRepository.java
+++ src/main/java/com/hello2morrow/sonarplugin/api/SonargraphRulesRepository.java
@@ -27,23 +27,32 @@ import com.hello2morrow.sonarplugin.foundation.SonargraphPluginBase;
 import java.util.Arrays;
 import java.util.List;
 
-public final class SonargraphRulesRepository extends RuleRepository
-{
-  public static final Rule ARCH = Rule.create(SonargraphPluginBase.PLUGIN_KEY, SonargraphPluginBase.ARCH_RULE_KEY, "Sonargraph Architecture Violation");
-  public static final Rule THRESHOLD = Rule.create(SonargraphPluginBase.PLUGIN_KEY, SonargraphPluginBase.THRESHOLD_RULE_KEY, "Sonargraph Threshold Violation");
-  public static final Rule DUPLICATES = Rule.create(SonargraphPluginBase.PLUGIN_KEY, SonargraphPluginBase.DUPLICATE_RULE_KEY, "Sonargraph Duplicate Code Block");
-  public static final Rule CYCLE_GROUPS = Rule.create(SonargraphPluginBase.PLUGIN_KEY, SonargraphPluginBase.CYCLE_GROUP_RULE_KEY, "Sonargraph Cycle Group");
-  public static final Rule WORKSPACE = Rule.create(SonargraphPluginBase.PLUGIN_KEY, SonargraphPluginBase.WORKSPACE_RULE_KEY, "Sonargraph Workspace Warning");
-  public static final Rule TASK = Rule.create(SonargraphPluginBase.PLUGIN_KEY, SonargraphPluginBase.TASK_RULE_KEY, "Sonargraph Task");
+public final class SonargraphRulesRepository extends RuleRepository {
 
-  public SonargraphRulesRepository()
-  {
+  public static final Rule ARCH = Rule.create(SonargraphPluginBase.PLUGIN_KEY, SonargraphPluginBase.ARCH_RULE_KEY,
+      "Sonargraph Architecture Violation").setDescription("Violation of the architecture defined using Sonargraph");
+  public static final Rule THRESHOLD = Rule.create(SonargraphPluginBase.PLUGIN_KEY,
+      SonargraphPluginBase.THRESHOLD_RULE_KEY, "Sonargraph Threshold Violation").setDescription(
+      "Violation of a threshold defined using Sonargraph");
+  public static final Rule DUPLICATES = Rule.create(SonargraphPluginBase.PLUGIN_KEY,
+      SonargraphPluginBase.DUPLICATE_RULE_KEY, "Sonargraph Duplicate Code Block").setDescription(
+      "Duplicate code block detected by Sonargraph");
+  public static final Rule CYCLE_GROUPS = Rule.create(SonargraphPluginBase.PLUGIN_KEY,
+      SonargraphPluginBase.CYCLE_GROUP_RULE_KEY, "Sonargraph Cycle Group").setDescription(
+      "Cycle group detected by Sonargraph");
+  public static final Rule WORKSPACE = Rule.create(SonargraphPluginBase.PLUGIN_KEY,
+      SonargraphPluginBase.WORKSPACE_RULE_KEY, "Sonargraph Workspace Warning").setDescription(
+      "Sonargraph Workspace Warning");
+  public static final Rule TASK = Rule
+      .create(SonargraphPluginBase.PLUGIN_KEY, SonargraphPluginBase.TASK_RULE_KEY, "Sonargraph Task")
+      .setDescription("").setDescription("Sonargraph Task");
+
+  public SonargraphRulesRepository() {
     super(SonargraphPluginBase.PLUGIN_KEY, Java.KEY);
   }
 
   @Override
-  public List<Rule> createRules()
-  {
+  public List<Rule> createRules() {
     return Arrays.asList(ARCH, THRESHOLD, TASK, CYCLE_GROUPS, WORKSPACE, DUPLICATES);
   }
 }
